1dc67384141b7e8bcb5b8824b545b6981795b187
Alexandra <little.runyan@gmail.com>
2019-03-08 08:45:31 -0800

    My first commit with starter files

---
+++ app.js
@@ -0,0 +1,25 @@
+//var url = "https://squarespace.zendesk.com/api/v2/help_center/en-us/categories/200290338/articles.json?"
+//
+//url = $.map(url, function(page, i) {
+//    return (page + "page=" (pageNumber + 1);
+//})
+//
+//_.each(url, function(page) {
+//  url[]
+//})
+
+
+//var map = {};
+//  _.each(sections, function(section){
+//    map[section.id] = section.category_id;
+//  });
+//  return map;
+//};
+
+pageNumber = 1;
+
+function page(number) {
+  pageNumber += 1++;
+}
+
+console.log(pageNumber);
